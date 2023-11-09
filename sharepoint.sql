804e14f4211cc87c3066a325ed87f0f4f89ce043
dreamsdreams <103957567+dreamsdreams@users.noreply.github.com>
2023-11-09 14:19:59 -0500

    Create sharepoint.sql

---
+++ sharepoint.sql
@@ -0,0 +1,98 @@
+drop table if exists csv2sql_9494 cascade;
+create table csv2sql_9494 (
+	id int not null auto_increment primary key, 
+	title varchar(64),
+	project_number varchar(64),
+	email varchar(64),
+	authors varchar(64),
+	class varchar(64),
+	roles varchar(64),
+	meeting_times varchar(64),
+	project_purpose varchar(64),
+	due_date varchar(64),
+	form_of_communication varchar(64),
+	challenges varchar(64)
+	
+);
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'try this' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'exp1' ,   NULL ,   'fff' ,   'ff' ,   'fdd' ,   'sdsdk' ,   'dsklk' ,   'sdlksld' ,   NULL ,   'slkdlk' ,   'sdlkld'   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'boo-Test1' ,   NULL ,   NULL ,   'him and her' ,   'cpsc' ,   'so many' ,   'everyday' ,   'lots' ,   '10/12/2023' ,   'some' ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'ti' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'ti' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  't' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'ca6' ,   NULL ,   NULL ,   NULL ,   NULL ,   'hit' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'hk' ,   NULL ,   NULL ,   NULL ,   'bit' ,   'hit' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'jkl' ,   NULL ,   NULL ,   NULL ,   'bit' ,   'hit' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'ol' ,   NULL ,   NULL ,   NULL ,   'bit' ,   'hit' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'koo.' ,   NULL ,   NULL ,   NULL ,   'bit' ,   'hit' ,   'fit' ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'mlom' ,   NULL ,   NULL ,   NULL ,   'bit' ,   'hit' ,   'fit' ,   'fd' ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'tit' ,   NULL ,   NULL ,   'buyfvj' ,   'bit' ,   'hit' ,   'fit' ,   'fd' ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  '1111' ,   NULL ,   NULL ,   '222' ,   '333' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  '2211' ,   NULL ,   NULL ,   '3322' ,   '443' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'Trial1' ,   NULL ,   NULL ,   'Trial1' ,   'Trial`1' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'Trial2' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'Trial3-Test1' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'Test4-Test1' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'hihn' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'example' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'testing for ' ,   NULL ,   NULL ,   'me' ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL ,   NULL   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'this is a test' ,   NULL ,   NULL ,   'olivia, cole, jordan, adam, hayden, parker' ,   'cpsc 4900' ,   'many roles ' ,   'tuesdays and thursdays' ,   'make an ap for students' ,   '12/1/2023' ,   'groupme, discord' ,   'a couple'   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'Test1' ,   NULL ,   NULL ,   'Parker Phillips' ,   'CPSC 4900' ,   'Scrum Master' ,   '1600-1900 MWF' ,   'Testing of Power Apps' ,   '12/1/2023' ,   'Oral ' ,   'We are about to find out.'   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'Test1' ,   NULL ,   NULL ,   'Parker Phillips' ,   'CPSC 4900' ,   'Scrum Master' ,   '1600-1900 MWF' ,   'Testing of Power Apps' ,   '12/1/2023' ,   'Oral ' ,   'We are about to find out.'   );
+
+insert into csv2sql_9494 (  title ,   project_number ,   email ,   authors ,   class ,   roles ,   meeting_times ,   project_purpose ,   due_date ,   form_of_communication ,   challenges   )
+	values (  'Test Project 1' ,   NULL ,   NULL ,   'Hayden Hunley' ,   'CPSC4900' ,   '1,2,3,4' ,   '12:00' ,   'To do a demonstration' ,   NULL ,   NULL ,   NULL   );
+
